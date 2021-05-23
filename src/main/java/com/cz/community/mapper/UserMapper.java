36f93ba5957737f661f12117b02581d1f2093831
caozheng0401 <1546376420@qq.com>
2021-05-23 23:39:29 +0800

    sss

---
+++ src/main/java/com/cz/community/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.cz.community.mapper;
+
+/**
+ * @author cz
+ * @create 2021/5/23 21:39
+ */
+public class UserMapper {
+}
---
+++ src/main/java/com/cz/community/model/User.java
@@ -0,0 +1,8 @@
+package com.cz.community.model;
+
+/**
+ * @author cz
+ * @create 2021/5/23 21:41
+ */
+public class User {
+}
